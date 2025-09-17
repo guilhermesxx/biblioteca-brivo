0f0ce676bbb4309cc78eee1765b89e3c3cf122d8
guilhermesxx <161722755+guilhermesxx@users.noreply.github.com>
2025-09-17 10:48:36 -0300

    Atualização Banco De Dados

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -187,15 +187,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # -----------------------------------------------------------------------------
 # Configurações de envio de e-mail
 # -----------------------------------------------------------------------------
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-EMAIL_TIMEOUT = 60
-EMAIL_HOST_USER = 'brivo1652@gmail.com'
-EMAIL_HOST_PASSWORD = 'dmkbymviegqjirmu'
+EMAIL_TIMEOUT = 30
+EMAIL_HOST_USER = ' '
+EMAIL_HOST_PASSWORD = 'cqtbqjyaktaiztzi'
 DEFAULT_FROM_EMAIL = 'Biblioteca Escolar <brivo1652@gmail.com>'
 
 # -----------------------------------------------------------------------------
